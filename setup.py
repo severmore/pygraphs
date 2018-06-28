3f40cbafe42005f0e6eb88cff8a94ce9af01b072
Roman Ivanov <iromcorp@gmail.com>
2018-06-28 17:56:03 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as rm:
+  readme = rm.read()
+
+with open('LICENSE') as li:
+  license = li.read()
+
+setup(
+  name = 'sample',
+  version = '0.0.1',
+  description = 'Algorithms on graphs',
+  long_description = readme,
+  author = 'Roman Ivanov',
+  auhtor_email = 'iromcorp@gmail.com',
+  url = 'https://github.com/severmore/pygraphs.git',
+  license = license,
+  packages = find_packages(exclude=('tests', 'docs'))
+)
---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestProject(unittest.TestCase):
+  """ Project existance test """
+
+  def test_if_project_is_ok(self):
+    assert True
+
+if __name__ == '__main__':
+  unittest.main()
