b7714843e970aec83e305b8a224173f52971eeee
Roman Ivanov <iromcorp@gmail.com>
2018-07-27 14:41:34 +0300

    Complete example of mesh-network

--- bgraphs/examples/mesh.py
+++ bgraphs/examples/mesh.py
@@ -3,10 +3,11 @@ import random
 import bgraphs.generating
 import bgraphs.spanning
 import bgraphs.coloring
+import bgraphs.graph
 
 if __name__ == '__main__':
 
-  vertices_num = 20  
+  vertices_num = 10  
 
   gen = bgraphs.generating.Geo(100, 200, (800, 800), (25,25))
   graph = gen(vertices_num)
@@ -14,14 +15,14 @@ if __name__ == '__main__':
   gen.show('scene')
   print('Valid?', gen.is_valid())
   print('edges', gen._edges)
-  print(graph)
+  print('initial', graph)
 
   spanning = bgraphs.spanning.SpanningBGraph(graph)
   init_coloring = tuple(random.choices([0,1], k=vertices_num))
   bgraph = spanning(init_coloring, vertices_num)
-  bgraph_con = spanning.to_conventional_form()
+  bgraph_con = spanning.reindex()
 
-  print(bgraph)
+  print('bipartite', bgraph)
   print(bgraph_con)
   print('is bipartite?', spanning.is_bipartite(bgraph))
   print('is bipartite?', spanning.is_bipartite(bgraph_con))
@@ -30,7 +31,21 @@ if __name__ == '__main__':
   coloring = bgraphs.coloring.colorize(bgraph)
   coloring_con = bgraphs.coloring.colorize(bgraph_con)
 
-  print(coloring)
+  # print(coloring)
   # print(coloring_con)
-  print('is valid?', bgraphs.coloring.is_valid(coloring, bgraph))
+  print('Vising is valid?', bgraphs.coloring.is_valid(coloring, bgraph))
   # print('is valid?', bgraphs.coloring.is_valid(coloring_con, bgraph_con))
+
+  graph_copy = bgraphs.graph.UDGraph(graph=bgraph)
+
+  print('coppied', graph_copy)
+  ch_coloring = bgraphs.coloring.colorize(graph_copy, algorithm='Cole-Hopcroft')
+
+  print('Cole-Hopcroft coloring', ch_coloring)
+
+  f_ch_coloring = bgraphs.coloring.ColeHopcroftColoring.to_commmon_form(
+        ch_coloring)
+  
+  print('formatted', f_ch_coloring)
+
+  print('Cole-Hopcroft is valid?', bgraphs.coloring.is_valid(coloring, bgraph))
