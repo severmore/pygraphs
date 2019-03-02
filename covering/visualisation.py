f17d2a73f73b56e4e3f245129453a29aa8d0a69b
timac11 <timac11@bk.ru>
2019-03-02 16:28:12 +0300

    refactoring visualisation

--- covering/visualisation.py
+++ covering/visualisation.py
@@ -10,8 +10,17 @@ def covering_visualisation(gen, stations, points):
     def get_circle(point, radius, color):
         return plt.Circle((point[0]*gen.cell[0], point[1]*gen.cell[0]), radius, fc=color)
 
-    for station in stations:
-        ax.add_artist(get_circle((station.get_x(), station.get_y()), station.radius, 'b'))
+    def get_blue_color(index):
+        return 0, 0, 1 - ((index + 1) / len(stations))
+
+    for i, station in enumerate(stations):
+        ax.add_artist(
+            get_circle(
+                (station.get_x(), station.get_y()),
+                station.radius,
+                get_blue_color(i)
+            )
+        )
 
     for point in points:
         ax.add_artist(get_circle(point, 1, 'r'))
