2b75eb296af085ad06cf0bdf931a67d80dd56782
timac11 <timac11@bk.ru>
2019-03-02 17:15:59 +0300

    change visualisation: add grid

--- covering/visualisation.py
+++ covering/visualisation.py
@@ -7,6 +7,23 @@ def covering_visualisation(gen, stations, points):
     ax.set_xlim(0, gen.area[0])
     ax.set_ylim(0, gen.area[1])
 
+    x_grid = list()
+    y_grid = list()
+
+    x_max = int(gen.area[0] // gen.grid[0])
+    y_max = int(gen.area[1] // gen.grid[1])
+
+    for i in range(0, x_max):
+        x_grid.append(gen.grid[0] * i)
+
+    for i in range(0, y_max):
+        y_grid.append(gen.grid[1] * i)
+
+    ax.set_xticks(x_grid)
+    ax.set_yticks(y_grid)
+
+    ax.grid(True)
+
     def get_circle(point, radius, color):
         return plt.Circle((point[0]*gen.cell[0], point[1]*gen.cell[0]), radius, fc=color)
 
@@ -23,7 +40,6 @@ def covering_visualisation(gen, stations, points):
         )
 
     for point in points:
-        ax.add_artist(get_circle(point, 1, 'r'))
-
+        ax.add_artist(get_circle(point, 3, 'r'))
 
     fig.savefig('../covering/visualisation/covering.png')
